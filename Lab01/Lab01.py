9fba107f682b09d60822be3274a7fe7adf91b62e
KhanhDuong187it14054 <duong.187it14054@vanlanguni.vn>
2021-09-16 23:04:13 +0700

    add Chuong3 cau 2

--- Lab01/Lab01.py
+++ Lab01/Lab01.py
@@ -8,3 +8,13 @@ print("max: ",b)
 # 1c 
 c = max(2, -3, min(4, 7), -5)
 print("max: ",c)
+
+#2a
+a = min(max(3, 4), abs(-5))
+print("min: ",a)
+#2b
+b = abs(min(4, 6, max(2, 8)))
+print("abs: ",b)
+#2c
+c = round(max(5.572, 3.258), abs(-2))
+print("round: ",c)
