6ada1e4714f55f877a7c7d143be1cb306cec2b87
KhanhDuong187it14054 <duong.187it14054@vanlanguni.vn>
2021-09-21 19:46:34 +0700

    add Chuong8 cau2

--- Lab02/Lab02.py
+++ Lab02/Lab02.py
@@ -2,7 +2,7 @@
 kingdoms = ['Bacteria', 'Protozoa', 'Chromista',
 'Plantae', 'Fungi','Animalia']
 print("kingdoms: ", kingdoms)
-# 1a
+#1a
 print("kingdoms[0]: ", kingdoms[0])
 #1b
 print("kingdoms[5]: ", kingdoms[5])
@@ -15,4 +15,15 @@ print("kingdoms[4:]: ", kingdoms[4:])
 #1f
 print("kingdoms[1:0]: ", kingdoms[1:0])
 
-
+#2a
+print("kingdoms[-6]: ", kingdoms[-6])
+#2b
+print("kingdoms[-1]: ", kingdoms[-1])
+#2c
+print("kingdoms[-6:-3]: ", kingdoms[-6:-3])
+#2d
+print("kingdoms[-4:-1]: ", kingdoms[-4:-1])
+#2e
+print("kingdoms[-2:]: ", kingdoms[-2:])
+#2f
+print("kingdoms[-1:-2]: ", kingdoms[-1:-2])
