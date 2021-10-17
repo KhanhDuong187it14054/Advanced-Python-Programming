8b2f74a78ea2aff79023cf90397ef19bc3c51c40
KhanhDuong187it14054 <duong.187it14054@vanlanguni.vn>
2021-10-17 21:03:25 +0700

    add Lab05 Chuong 14 Cau 1

---
+++ Lab05/Country.py
@@ -0,0 +1,20 @@
+class Country:
+    def __init__(self, name, population, area):
+        self.name = name
+        self.population = population
+        self.area = area
+
+    def is_larger(self, objCountry):
+        if self.area >= objCountry.area:
+            return True
+        else:
+            return False
+
+    def population_density(self):
+        return self.population / self.area
+    
+    def __str__(self):
+        return '{} has a population of {} and is {} square km.'.format(self.name, self.population, self.area)
+
+    def __repr__(self):
+        return "Country('{0}', {1}, {2})".format(self.name, self.population, self.area)
